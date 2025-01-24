f984f2c664b4f0578f6b3db8e441dac9d99db7a5
papsykle <toramgarcia@gmail.com>
2025-01-24 20:49:53 +0800

    added the add deck name activity

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,12 @@ void main() {
   runApp(const MyApp());
 }
 
+const Color finalBackgroundColor = Color.fromRGBO(40, 42, 54, 1.0);
+const Color finalFontColor = Color.fromRGBO(255, 249, 102, 1.0);
+const Color finalFabColor = Color.fromRGBO(40, 42, 54, 0.5);
+const Color finalHintColor = Color.fromRGBO(255, 249, 102, 0.5);
+
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -27,23 +33,22 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'Edukado Manzano',
+          'Edukado Manzano'
         ),
-        centerTitle: true,
-        titleTextStyle: const TextStyle(
+          titleTextStyle: const TextStyle(
           fontSize: 24,
-          color: Color.fromRGBO(255, 249, 102, 1.0),
+          color: finalFontColor,
         ),
-        backgroundColor: const Color.fromRGBO(40, 42, 54, 1.0),
+        backgroundColor: finalBackgroundColor,
       ),
-      backgroundColor: const Color.fromRGBO(40, 42, 54, 1.0),
+      backgroundColor: finalBackgroundColor,
       body: ListView(
           children: const <Widget>[
             ListTile(
               title: Text(
                 "MIS",
                 style: TextStyle(
-                    color: Color.fromRGBO(255, 249, 102, 1.0)
+                    color: finalFontColor
                 ),
               ),
             ),
@@ -56,23 +61,130 @@ class HomeScreen extends StatelessWidget {
               title: Text(
                 "Testing",
                 style: TextStyle(
-                    color: Color.fromRGBO(255, 249, 102, 1.0)
+                    color: finalFontColor
                 ),
               ),
             ),
           ], // Children
       ),
       floatingActionButton: FloatingActionButton(
-        backgroundColor: const Color.fromRGBO(40, 42, 54, 0.5),
+        backgroundColor: finalFabColor,
         tooltip: "Increment",
         child: const Icon(
           Icons.add,
-          color: Color.fromRGBO(255, 249, 102, 1.0),
+          color: finalFontColor,
           size: 50,
         ),
-        onPressed: (){},
+        onPressed: (){
+          Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const CreateDeck())
+          );
+        },
       ),
     );
+  }
+}
+
+class CreateDeck extends StatelessWidget {
+  const CreateDeck({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Create Deck"),
+        backgroundColor: finalBackgroundColor,
+        titleTextStyle: const TextStyle(
+          fontSize: 24,
+          color: finalFontColor,
+        ),
+      ),
+      backgroundColor: finalBackgroundColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            deckLabel(),
+            spaceBetween(10),
+            deckNameTextBox(),
+            spaceBetween(60),
+            proceedButton()
+          ],
+        ),
+      )
+    );
+  }
+
+  Text deckLabel() {
+    return const Text(
+      "Enter deck name",
+      style: TextStyle(
+          color: finalFontColor,
+          fontSize: 24,
+          fontWeight: FontWeight.bold
+      ),
+    );
+  }
+
+  SizedBox proceedButton() {
+    return SizedBox(
+      height: 50,
+      width: 400,
+      child: ElevatedButton(
+        onPressed: (){ },
+        style: ButtonStyle(
+            backgroundColor: const WidgetStatePropertyAll(finalFontColor),
+            shape: WidgetStatePropertyAll(
+                RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0)
+                )
+            )
+        ),
+        child: const Text(
+          "Proceed",
+          style: TextStyle(
+              color: finalBackgroundColor
+          ),
+        ),
+      )
+    );
+  }
+
+  SizedBox deckNameTextBox() {
+    return const SizedBox(
+      width: 400,
+      child: TextField(
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                  color: finalFontColor,
+                  width: 1.5
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(10.0))
+          ),
+          focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                  color: finalFontColor,
+                  width: 1.5
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(10.0))
+          ),
+          hintText: "Deck name",
+          focusColor: finalFontColor,
+          hintStyle: TextStyle(
+              color: finalHintColor
+          ),
+        ),
+        style: TextStyle(
+            color: finalFontColor
+        ),
+      ),
+    );
+  }
+
+  SizedBox spaceBetween(double space) {
+    return SizedBox(height: space);
   }
 }
 
